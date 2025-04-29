0bcaa21cb451c85146a2f81c94c506a7396d7c6b
unknown <barasatyaradi@gmail.com>
2025-04-29 16:20:24 +0700

    Add Locale

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
 
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        config(['app.locale' => 'id']);
+        Carbon::setLocale('id');
+
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }

286f012da6f7bf330860a4aa32cde719425dda6c
unknown <barasatyaradi@gmail.com>
2025-05-06 18:06:23 +0700

    add line for move ccmd first on .bat

--- db-delete-old-data.bat
+++ db-delete-old-data.bat
@@ -1,2 +1,5 @@
 @echo off
+REM Pindah ke direktori tempat .bat ini berada
+cd /d %~dp0
+
 php artisan db:delete-old-data
--- logs-clear.bat
+++ logs-clear.bat
@@ -1,2 +1,6 @@
 @echo off
+
+REM Pindah ke direktori tempat .bat ini berada
+cd /d %~dp0
+
 php artisan log:clear

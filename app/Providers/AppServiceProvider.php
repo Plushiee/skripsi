e949d80b7f8a6fd71df212b85d6b0e4924355fdc
Plushiee <barasatyaradi@gmail.com>
2025-07-20 16:26:43 +0700

    Feat: Tempfix timezone setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         config(['app.locale' => 'id']);
         Carbon::setLocale('id');
+        date_default_timezone_set('Asia/Jakarta');
 
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
